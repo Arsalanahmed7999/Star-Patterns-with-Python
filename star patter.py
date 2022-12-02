9a5d5e345371f2ab018ab60cba738e03e99a8567
Arsalan <arsalanahmed7999@gmail.com>
2022-12-03 03:17:45 +0530

    first commit

--- star patter.py
+++ star patter.py
@@ -152,42 +152,27 @@
 *                     * 
 
 '''
-n = int(input('Enter the value n: \n'))
-for i in range(n - 1):
-  for j in range(i+1):
-    print('*', end=' ')
-  for j in range(i, n):
-    print(' ', end=' ')
-  for j in range(i, n):
-    print(' ', end=' ')
-  for j in range(i + 1):
-    print('*', end=' ')
-  print()
-for i in range(n):
-  for j in range(i, n):
-    print('*', end=' ')
-  for j in range(i + 1):
-    print(' ', end=' ')
-  for j in range(i + 1):
-    print(' ', end=' ')
-  for j in range(i, n):
-    print('*', end=' ')
-  print()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# n = int(input('Enter the value n: \n'))
+# for i in range(n - 1):
+#   for j in range(i+1):
+#     print('*', end=' ')
+#   for j in range(i, n - 1):
+#     print(' ', end=' ')
+#   for j in range(i, n -1):
+#     print(' ', end=' ')
+#   for j in range(i + 1):
+#     print('*', end=' ')
+#   print()
+# for i in range(n):
+#   for j in range(i, n):
+#     print('*', end=' ')
+#   for j in range(i):
+#     print(' ', end=' ')
+#   for j in range(i):
+#     print(' ', end=' ')
+#   for j in range(i, n):
+#     print('*', end=' ')
+#   print()
 
 # Sandglass Pattern
 '''
@@ -202,3 +187,37 @@ for i in range(n):
     * * * * * * * 
   * * * * * * * * * 
 '''
+
+# n = int(input('Enter the value n: \n'))
+# # n = 5
+
+# for i in range(n):
+
+#   for j in range(i+1):
+#     print(' ', end=' ')
+#   for j in range(i, n-1):
+#     print('*', end=' ')
+#   for j in range(i, n):
+#     print('*', end=' ')
+#   print()
+# for i in range(n):
+
+#   for j in range(i ,n):
+#     print(' ', end=' ')
+#   for j in range(i+1):
+#     print('*', end=' ')
+#   for j in range(i):
+#     print('*', end=' ')
+#   print()
+
+
+# Double Hill Pattern
+'''
+
+          *                 * 
+        * * *             * * * 
+      * * * * *         * * * * * 
+    * * * * * * *     * * * * * * * 
+  * * * * * * * * * * * * * * * * * * 
+'''
+
