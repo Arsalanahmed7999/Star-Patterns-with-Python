e187d29fda4665e1e7ecc15f4e8acb8e9862ebc1
Arsalan <arsalanahmed7999@gmail.com>
2022-12-07 03:27:44 +0530

    first commit

--- star patter.py
+++ star patter.py
@@ -1,3 +1,121 @@
+
+
+# n = 5 
+# # Square Pattern
+# '''
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# '''
+# for i in range(n):
+#     for j in range(n):
+#         print('*', end=" ")
+#     print() 
+
+
+# Increasing Triangle Pattern 
+
+"""
+*
+**
+***
+****
+*****
+"""
+# for i in range(n): #counting rows
+#     for j in range(i+1): #counting columns
+#         print('*', end=" ")
+#     print()
+
+
+# Decreasing Triangle
+
+'''
+*****
+****
+***
+**
+*
+'''
+# for i in range(n): #counting rows
+#     for j in range(i, n): #counting columns
+#         print('*', end=" ")
+#     print()
+
+# Right sided triangle
+'''
+    *
+   **
+  ***
+ ****
+*****
+'''
+# n = 5
+# for i in range(n): #counting rows
+#     for j in range(i, n): #counting columns
+#         print(' ', end=" ")
+#     for j in range(i+1): #counting columns
+#         print('*', end=" ")
+#     print()
+
+# Reverse Right sided triangle
+'''
+*****
+ ****
+  ***
+   **
+    *
+'''
+# n = 5
+# for i in range(n): #counting rows
+#     for j in range(i+1): #counting columns
+#         print(' ', end=" ")
+#     for j in range(i, n): #counting columns
+#         print('*', end=" ")
+#     print()
+
+# Hill Pattern
+"""
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+"""
+# n = 5
+
+# for i in range(n): #counting rows
+#     for j in range(i, n): #counting columns
+#         print(' ', end=" ")
+#     for j in range(i+1): #counting columns
+#         print('*', end=" ")
+#     for j in range(i): #counting columns
+#         print('*', end=" ")
+#     print()
+
+
+# Reverse Hill Pattern
+'''
+  * * * * * * * * * 
+    * * * * * * * 
+      * * * * * 
+        * * * 
+          * 
+'''
+# n = 5
+# for i in range(n): #counting rows
+#     for j in range(i + 1): #counting columns
+#         print(' ', end=" ")
+#     for j in range(i, n - 1): #counting columns
+#         print('*', end=" ")
+#     for j in range(i, n): #counting columns
+#         print('*', end=" ")
+#     print()
+
+# Left 
+
 # Increasing Triangle Pattern 
 
 """
@@ -150,7 +268,6 @@
 * * *             * * * 
 * *                 * * 
 *                     * 
-
 '''
 # n = int(input('Enter the value n: \n'))
 # for i in range(n - 1):
@@ -213,11 +330,89 @@
 
 # Double Hill Pattern
 '''
-
           *                 * 
         * * *             * * * 
       * * * * *         * * * * * 
     * * * * * * *     * * * * * * * 
   * * * * * * * * * * * * * * * * * * 
 '''
+# n = 5
+# n = int(input('Enter the value n: \n'))
 
+
+# for i in range(n):
+#   for j in range(i, n):
+#     print(' ', end=' ')
+#   for j in range(i + 1):
+#     print('*', end=' ')
+#   for j in range(i):
+#     print('*', end=' ')
+#   for j in range(i, n -1):
+#     print(' ', end=' ')
+#   for j in range(i, n - 1):
+#     print(' ', end=' ')
+#   for j in range(i + 1):
+#     print('*', end=' ')
+#   for j in range(i):
+#     print('*', end=' ')   
+#   print()
+
+
+
+
+
+
+
+
+# Right sided pascals triangle
+'''
+* 
+* * 
+* * * 
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+'''
+
+# n = 5
+n = int(input('Enter the value n: \n'))
+
+for i in range(n - 1):
+  for j in range(i+1):
+    print('*', end=' ')
+  print()
+
+for i in range(n):
+  for j in range(i, n):
+    print('*', end=' ')
+  print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Left sided pascals triangle
+'''
+          * 
+        * * 
+      * * * 
+    * * * * 
+  * * * * * 
+    * * * * 
+      * * * 
+        * * 
+          * 
+'''
